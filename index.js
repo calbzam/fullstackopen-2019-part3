b5c5a8fc2353ef6273ef4d86c89da2998d86bce2
Carlos Alba <calbzam@MacBook-Pro-de-Carlos.local>
2019-11-07 00:38:05 +0100

    Add frontend build folder

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ let persons = [
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('build'))
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
