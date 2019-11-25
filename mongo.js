c52b8b76752235709ee238be830ed9568043d7b1
calbzam <albazamanillo@gmail.com>
2019-11-25 23:37:40 +0100

    =minor changes

--- mongo.js
+++ mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const url =
   `mongodb+srv://fullstack:fullstack@cluster0-5uh6a.mongodb.net/test?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
 
 
